cb3ad4c2f119f55a94f159a08c3847757aba0907
wadi2a <wadica2@hotmail.fr>
2016-02-24 03:02:51 +0100

    metre le titre de l'index en majiscule

--- index.php
+++ index.php
@@ -16,8 +16,7 @@
 	<head>
 		<meta charset="UTF-8"/>
 		<link rel="stylesheet" href="includes/style.css" type="text/css" />
-		<title> Ben Aissa Ouadie</title>
-		<!--[if lt IE 9]>
+		<title>BEN AISSA OUADIE</title>		<!--[if lt IE 9]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
 	</head>
